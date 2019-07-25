fc6ee259743e3c32c10b1c94bd3664156ecc999a
Canersoz02 <41192007+Canersoz02@users.noreply.github.com>
2019-07-25 16:40:07 +0300

    Update sensorOptimise.py

--- sensorOptimise.py
+++ sensorOptimise.py
@@ -6,8 +6,6 @@ import array
 import time
 from std_msgs.msg import Float32
 
-
-
 class SensData:
     def __init__(self):
         self.data = 1
@@ -21,27 +19,21 @@ class SensData:
 
 def stdDev(arr):
     n = len(arr)
-
     mean, sd = avg(arr), 0.0
-
     for i in arr:
         sd += (float(i) - mean)**2
     sd = math.sqrt(sd / float(n-1))
-
     if(sd > 4 or lowest(arr) > 640):
         return 0
     return lowest(arr)
 
 def avg(arr):
     n, mean = len(arr), 0.0
-
     if n <= 1:
         return ls[0]
-
     for i in arr:
         mean = mean + float(i)
     mean = mean / float(n)
-
     return mean
 
 def lowest(arr):
@@ -54,15 +46,10 @@ def lowest(arr):
     return(lst)
 
 def inRange(arr):
-
     for i in arr:
         if(i > 0 and i < 20 or i > 644):
             return(True)
     
-
-
-
-
 if __name__ == '__main__':
     arr = []
     a = SensData()
